df771a2326f89bbf388b4cfdf6c8ad31e1918b25
Tarpelite <tarpelite_chan@foxmail.com>
2020-04-05 16:04:31 +0800

    debug

--- examples/ner/run_ner_strain.py
+++ examples/ner/run_ner_strain.py
@@ -81,6 +81,8 @@ def train(args, train_dataset, model, tokenizer, labels, pad_token_label_id):
     else:
         t_total = len(train_dataloader) // args.gradient_accumulation_steps * args.num_train_epochs
 
+    if args.warmup_ratio > 0:
+        args.warmup_steps = int(t_total * args.warmup_ratio)
     # Prepare optimizer and schedule (linear warmup and decay)
     no_decay = ["bias", "LayerNorm.weight"]
     optimizer_grouped_parameters = [
@@ -480,6 +482,7 @@ def main():
 
     parser.add_argument("--logits_file", type=str, default="")
     parser.add_argument("--eval_file", type=str, default="")
+    parser.add_argument("--warmup_ratio", type=float, default=0.1)
 
     parser.add_argument(
         "--eval_all_checkpoints",
