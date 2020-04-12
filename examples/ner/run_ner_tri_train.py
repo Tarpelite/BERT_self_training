03ab93e436cf20f18b2b884de7241e14bdf55c12
Tarpelite <tarpelite_chan@foxmail.com>
2020-04-12 20:34:29 +0800

    debug

--- examples/ner/run_ner_tri_train.py
+++ examples/ner/run_ner_tri_train.py
@@ -436,10 +436,10 @@ def train_ft(args, model_ft, train_dataset):
     no_decay = ["bias", "LayerNorm.weight"]
     optimizer_grouped_parameters = [
         {
-            "params": [p for n, p in list(model_f1.named_parameters()) + list(model_f2.named_parameters()) if not any(nd in n for nd in no_decay)],
+            "params": [p for n, p in list(model_ft.named_parameters()) if not any(nd in n for nd in no_decay)],
             "weight_decay": args.weight_decay,
         },
-        {"params": [p for n, p in list(model_f1.named_parameters()) + list(model_f2.named_parameters()) if any(nd in n for nd in no_decay)], "weight_decay": 0.0},
+        {"params": [p for n, p in list(model_ft.named_parameters())  if any(nd in n for nd in no_decay)], "weight_decay": 0.0},
     ] 
 
     optimizer = AdamW(optimizer_grouped_parameters, lr=args.learning_rate, eps=args.adam_epsilon)
