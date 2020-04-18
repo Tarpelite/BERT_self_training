de7e6f6854b1d82f922ff112c72366fe231b8b7b
Tarpelite <tarpelite_chan@foxmail.com>
2020-04-18 17:28:32 +0800

    debug

--- examples/ner/run_ner_tri_train.py
+++ examples/ner/run_ner_tri_train.py
@@ -562,12 +562,10 @@ def evaluate(args, model, tokenizer, labels, pad_token_label_id, mode, prefix=""
             inputs = {
                 "input_ids": batch[0], 
                 "attention_mask": batch[1], 
+                "token_type_ids":batch[2],
                 "labels": batch[3],
                 "label_mask":batch[4]}
-            if args.model_type != "distilbert":
-                inputs["token_type_ids"] = (
-                    batch[2] if args.model_type in ["bert", "xlnet"] else None
-                )  # XLM and RoBERTa don"t use segment_ids
+         
             outputs = model(**inputs)
             tmp_eval_loss, logits = outputs[:2]
 
@@ -611,56 +609,6 @@ def evaluate(args, model, tokenizer, labels, pad_token_label_id, mode, prefix=""
     return results
 
 
-def load_and_cache_examples(args, tokenizer, labels, pad_token_label_id, mode):
-    if args.local_rank not in [-1, 0] and not evaluate:
-        torch.distributed.barrier()  # Make sure only the first process in distributed training process the dataset, and the others will use the cache
-
-    # Load data features from cache or dataset file
-    cached_features_file = os.path.join(
-        args.data_dir,
-        "cached_{}_{}_{}".format(
-            mode, list(filter(None, args.model_name_or_path.split("/"))).pop(), str(args.max_seq_length)
-        ),
-    )
-    if os.path.exists(cached_features_file) and not args.overwrite_cache:
-        logger.info("Loading features from cached file %s", cached_features_file)
-        features = torch.load(cached_features_file)
-    else:
-        logger.info("Creating features from dataset file at %s", args.data_dir)
-        examples = read_examples_from_file(args.data_dir, mode)
-        features = convert_examples_to_features(
-            examples,
-            labels,
-            args.max_seq_length,
-            tokenizer,
-            cls_token_at_end=bool(args.model_type in ["xlnet"]),
-            # xlnet has a cls token at the end
-            cls_token=tokenizer.cls_token,
-            cls_token_segment_id=2 if args.model_type in ["xlnet"] else 0,
-            sep_token=tokenizer.sep_token,
-            sep_token_extra=bool(args.model_type in ["roberta"]),
-            # roberta uses an extra separator b/w pairs of sentences, cf. github.com/pytorch/fairseq/commit/1684e166e3da03f5b600dbb7855cb98ddfcd0805
-            pad_on_left=bool(args.model_type in ["xlnet"]),
-            # pad on the left for xlnet
-            pad_token=tokenizer.pad_token_id,
-            pad_token_segment_id=tokenizer.pad_token_type_id,
-            pad_token_label_id=pad_token_label_id,
-        )
-        if args.local_rank in [-1, 0]:
-            logger.info("Saving features into cached file %s", cached_features_file)
-            torch.save(features, cached_features_file)
-
-    if args.local_rank == 0 and not evaluate:
-        torch.distributed.barrier()  # Make sure only the first process in distributed training process the dataset, and the others will use the cache
-
-    # Convert to Tensors and build dataset
-    all_input_ids = torch.tensor([f.input_ids for f in features], dtype=torch.long)
-    all_input_mask = torch.tensor([f.input_mask for f in features], dtype=torch.long)
-    all_segment_ids = torch.tensor([f.segment_ids for f in features], dtype=torch.long)
-    all_label_ids = torch.tensor([f.label_ids for f in features], dtype=torch.long)
-
-    dataset = TensorDataset(all_input_ids, all_input_mask, all_segment_ids, all_label_ids)
-    return dataset
 
 def test(args, model, tokenizer, labels, pad_token_label_id, mode, prefix=""):
     eval_examples = args.processor.get_sep_twitter_test_examples(args.data_dir)
@@ -700,12 +648,10 @@ def test(args, model, tokenizer, labels, pad_token_label_id, mode, prefix=""):
             inputs = {
                 "input_ids": batch[0], 
                 "attention_mask": batch[1], 
+                "token_type_ids":batch[2],
                 "labels": batch[3],
                 "label_mask":batch[4]}
-            if args.model_type != "distilbert":
-                inputs["token_type_ids"] = (
-                    batch[2] if args.model_type in ["bert", "xlnet"] else None
-                )  # XLM and RoBERTa don"t use segment_ids
+            
             outputs = model(**inputs)
             tmp_eval_loss, logits = outputs[:2]
 
